6aa4c08a6ffc2ca629882ecc4287c30c2eceeb24
hussamadil <hussam0683@gmail.com>
2021-11-21 05:58:02 +0200

    user can set email and password and course URL dynamically

--- main.py
+++ main.py
@@ -1,36 +1,46 @@
 from  selenium import webdriver 
 import time
 import sys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 ChromeDriverPATH = "/home/hussamadil/Desktop/chromedriver" #change it base on your chromedriver path  
 
 driver = webdriver.Chrome(ChromeDriverPATH)
 
-#login 
+#config
 
-driver.get("https://codecourse.com/auth/signin?redirect=%2Fcourses%2Frealtime-private-messages-with-laravel-livewire")
+course_url = input('course url ? ')
+
+number_of_videos = input('How videos do you want to download ? ')
+
+user_email = input(' Your email ? ')
+
+user_password = input(' Your password ? ')
+
+# login process
+
+driver.get("https://codecourse.com/auth/signin")
 
 email = driver.find_element_by_id("email")
 
 password = driver.find_element_by_id("password")
 
-email.send_keys("me@hussamadil.com")
+email.send_keys(user_email)
 
-password.send_keys("mypassword")
+password.send_keys(user_password)
 
 driver.find_element_by_tag_name("button").click()
 
-driver.implicitly_wait(10)
+WebDriverWait(driver, 10).until(EC.url_to_be("https://codecourse.com/dashboard"))
 
-number_of_videos = sys.argv[1] #set loop scope for video to be download
+driver.implicitly_wait(5)
 
-for x in range(1, int(number_of_videos)):
+driver.get(course_url)
+
+#download
+
+for x in range(0, int(number_of_videos)):
         links = driver.find_elements_by_class_name('opacity-25')[x].find_element_by_tag_name('a')
         links.click()
         time.sleep(10)
-              
-
-                 
-
-
- 
---
+++ test.py
@@ -0,0 +1,5 @@
+number_of_videos = input('How videos do you want to download ? ')
+
+for x in range(0, int(number_of_videos)):
+
+    print(x)
