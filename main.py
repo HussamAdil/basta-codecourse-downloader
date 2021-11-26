27f00be899bdd2c7a1b39fbd45f9b987ca884f76
hussamadil <hussam0683@gmail.com>
2021-11-26 21:37:04 +0200

    chrome driver will be installed base on OS and user browser ,without hard coding path or get it from user

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
 
 import click
 
-
-@click.command(name= "download")
-@click.option("--chrome-path", default="/home/hussamadil/Desktop/chromedriver", help="Chrome web driver for Seliunm. Download from here: https://chromedriver.chromium.org/downloads/")
+@click.command(name="download")
 @click.option("--course-url", help="Please enter the course url.")
 @click.option("--number-of-videos", help= "Enter the number of videos to be downloaded.", default=1)
 @click.option("--email", help="The email you used to sign up for Codecourse website")
 @click.option("--password", help="The password you used to sign up for Codecourse website")
-def main(chrome_path, course_url, number_of_videos, email, password):
+def main(course_url, number_of_videos, email, password):
     """A small script to help you download videos from CodeCourse"""
     
-    driver = webdriver.Chrome(chrome_path)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
 
     # go to login url  process
     driver.get("https://codecourse.com/auth/signin")
@@ -24,7 +23,7 @@ def main(chrome_path, course_url, number_of_videos, email, password):
     web_email = driver.find_element_by_id("email")
     web_password = driver.find_element_by_id("password")
     
-    #submit login form 
+    # submit login form
     web_email.send_keys(email)
     web_password.send_keys(password)
     driver.find_element_by_tag_name("button").click()
